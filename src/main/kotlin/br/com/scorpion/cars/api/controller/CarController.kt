8724f478d540e5e7c076ef4f00623664a1de3e0b
diascoder <isabellydias29@gmail.com>
2024-02-08 15:00:48 -0300

    listar carro por id e atualizacao de dados de um carro

--- src/main/kotlin/br/com/scorpion/cars/api/controller/CarController.kt
+++ src/main/kotlin/br/com/scorpion/cars/api/controller/CarController.kt
@@ -6,12 +6,17 @@ import org.hibernate.engine.jdbc.Size
 import org.springframework.data.domain.Page
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 @RequestMapping("/cars")
@@ -30,5 +35,17 @@ class CarController(val carService: CarService) {
     ): ResponseEntity<Page<Car>> {
         val carsPage = carService.getCars(page, size);
         return ResponseEntity.ok(carsPage);
+    }
+
+    @GetMapping("/{id}")
+    fun getCarById(@PathVariable id: Long): ResponseEntity<Optional<Car>> {
+        val car = carService.getCarById(id);
+        return ResponseEntity.ok(car);
+    }
+
+    @PutMapping
+    fun updateCar(@RequestBody car: Car): ResponseEntity<Car> {
+        val updatedCar = carService.updateCar(car);
+        return ResponseEntity(updatedCar, HttpStatus.OK);
     }
 }
--- src/main/kotlin/br/com/scorpion/cars/api/service/CarService.kt
+++ src/main/kotlin/br/com/scorpion/cars/api/service/CarService.kt
@@ -6,15 +6,28 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
 import org.springframework.stereotype.Service
+import java.util.Optional
 
 @Service
 class CarService(val carRepository: CarRepository) {
     fun createCar(car: Car): Car {
-        return carRepository.save(car);
+        return carRepository.save(car)
     }
 
     fun getCars(pageNumber: Int, pageSize: Int): Page<Car> {
         val pageRequest: PageRequest = PageRequest.of(pageNumber, pageSize, Sort.by("id").ascending())
-        return carRepository.findAll(pageRequest);
+        return carRepository.findAll(pageRequest)
+    }
+
+    fun getCarById(id: Long): Optional<Car> {
+        return carRepository.findById(id)
+    }
+
+    fun updateCar(car: Car): Car {
+        return carRepository.save(car)
+    }
+
+    fun deleteCar(id: Long) {
+        carRepository.deleteById(id);
     }
 }
